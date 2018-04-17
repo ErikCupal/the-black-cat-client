85aa9cf00702ca74b879ce7b214bd669b3e23ce9
Erik Cupal <erik@cup.al>
2018-04-17 02:09:29 +0200

    WIP

--- core/src/com/erikcupal/theblackcatclient/core/GameCore.kt
+++ core/src/com/erikcupal/theblackcatclient/core/GameCore.kt
@@ -16,14 +16,14 @@ import com.erikcupal.theblackcatclient.state.stateUpdaterFactory
 import com.erikcupal.theblackcatclient.types.*
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
-import io.socket.client.IO
-import io.socket.client.Socket
-import io.socket.client.Socket.*
 import kotlinx.coroutines.experimental.CommonPool
 import kotlinx.coroutines.experimental.launch
 import org.json.JSONObject
 import rx.subjects.PublishSubject
 import rx.subjects.SerializedSubject
+import java.io.*
+import java.net.*
+import kotlin.concurrent.thread
 
 /**
  * GameCore
@@ -53,7 +53,7 @@ class GameCore : ApplicationAdapter() {
   val send = Send { payload -> sendMessageToServer(payload) }
 
   private var socket: Socket? = null
-  private val socketOptions = createSocketIoOptions()
+  private var outStream: DataOutputStream? = null
 
   lateinit var batch: SpriteBatch
 
@@ -99,7 +99,13 @@ class GameCore : ApplicationAdapter() {
       when (message) {
         is CONNECT           -> initializeSocket(message.server)
         is GAME_SET_STAGE    -> changeStage(message.stageName)
-        is GAME_LEAVE_SERVER -> socket?.disconnect()
+        is GAME_LEAVE_SERVER -> try {
+          socket?.close()
+        } catch (e: Exception) {
+          log("🕊️🕊️🕊️ SOCKET FAILED TO DISCONNECT 🕊️🕊️🕊️")
+          socket = null
+          outStream = null
+        }
       }
     }
   }
@@ -127,92 +133,128 @@ class GameCore : ApplicationAdapter() {
 
   private fun initializeSocket(server: String) {
     try {
-      socket = IO.socket(server, socketOptions)
-      socket?.connect()
+      dispatch..CONNECTING()
 
-      socket?.on(EVENT_CONNECT) {
-        log("🕊️ socket CONNECTED", server)
+      val serverAddressSplit: List<String> = server.split(":")
+      val ip = serverAddressSplit[0]
+      val port = serverAddressSplit[1].toInt()
+      socket = Socket(ip, port)
+
+      if (socket != null) {
+        val inStream = DataInputStream(socket!!.getInputStream())
+        outStream = DataOutputStream(socket!!.getOutputStream())
+
+        log("🕊️ socket CONNECTED")
         dispatch..CONNECTED()
-      }
-      socket?.on(EVENT_CONNECTING) {
-        log("🕊️ socket CONNECTING", server)
-        dispatch..CONNECTING()
-      }
-      socket?.on(EVENT_DISCONNECT) {
-        log("🕊️ socket DISCONNECTED", server)
-        socket = null
-        dispatch..DISCONNECTED()
-      }
-      socket?.on("MESSAGE") { data ->
-        onSocketMessage(data)
-      }
 
-      socket?.on(EVENT_CONNECT_ERROR) {
-        log("🕊️ socket CONNECT ERROR", server)
-        dispatch..CONNECT_FAILED()
-        socket = null
+        launch(CommonPool) {
+          try {
+            while(true) {
+              outStream?.writeUTF("HEARTBEAT")
+              Thread.sleep(1000)
+            }
+          } catch (e: Exception) {
+            log("🕊️ Heartbeat sending failed")
+          }
+        }
+
+        launch(CommonPool) {
+          try {
+            while(socket?.isConnected == true) {
+                val message = inStream.readUTF()
+                log("Message read:", message)
+
+                if (message == "DISCONNECT") {
+                  log("🕊️ socket DISCONNECTED", server)
+                  socket = null
+                  dispatch..DISCONNECTED()
+                } else {
+                  try {
+                    onSocketMessage(message)
+                  } catch (e: Exception) {
+                    log(e.toString())
+                    log("🕊️ could not parse message")
+                  }
+                }
+            }
+          } catch (e: Exception) {
+            log("🕊️ socket connection lost")
+            log(e.toString())
+
+            socket = null
+            outStream = null
+
+            dispatch..DISCONNECTED()
+          }
+        }
       }
     } catch (e: Exception) {
       log("🕊️🕊️🕊️ SOCKET FAILED TO CONNECT - INVALID SERVER ADDRESS 🕊️🕊️🕊️")
       dispatch..CONNECT_FAILED()
+      socket = null
+      outStream = null
     }
   }
 
   private fun sendMessageToServer(payload: Any) {
-    if (socket != null && socket!!.connected()) {
-      launch(CommonPool) {
-        val jsonMessage = """{"type": "${payload::class.simpleName}",""" + jsonMapper.writeValueAsString(payload).removeRange(0..0)
-        socket?.emit("MESSAGE", jsonMessage)
-        log("🦄 SOCKET_MESSAGE_SEND", payload.toString())
+    if (socket != null && socket!!.isConnected()) {
+      try {
+        thread(start = true) {
+          val messageObject = JSONObject()
+          messageObject.put("actionType", payload::class.simpleName)
+          messageObject.put("payload", JSONObject(jsonMapper.writeValueAsString(payload)))
+          val message = messageObject.toString()
+          outStream?.writeUTF(message)
+          log("🦄 SOCKET_MESSAGE_SEND", payload.toString())
+          log("🦄 SOCKET_MESSAGE_SEND (literal string)", message)
+        }
+      } catch(e: Exception) {
+        log(e.toString())
       }
     }
   }
 
-  private fun onSocketMessage(data: Array<Any>) {
-    if (data.isNotEmpty()) {
+  private fun onSocketMessage(messageString: String) {
+    val jsonMessage = JSONObject(messageString)
+    val messageType = jsonMessage.getString("actionType")
+    val jsonPayload = jsonMessage.getJSONObject("payload").toString()
 
-      val socketMessage = data[0] as JSONObject
-      val messageType = socketMessage.getString("type")
-      socketMessage.remove("type")
-      val jsonPayload = socketMessage.toString()
+    val deserializedMessage: Any? = when (messageType) {
+      CONNECTION_CONFIRMATION::class.simpleName  -> jsonMapper.readValue<CONNECTION_CONFIRMATION>(jsonPayload)
+      REGISTERED::class.simpleName               -> jsonMapper.readValue<REGISTERED>(jsonPayload)
+      NAME_TAKEN::class.simpleName               -> jsonMapper.readValue<NAME_TAKEN>(jsonPayload)
+      ROOM_CREATED::class.simpleName             -> jsonMapper.readValue<ROOM_CREATED>(jsonPayload)
+      ROOM_NAME_TAKEN::class.simpleName          -> jsonMapper.readValue<ROOM_NAME_TAKEN>(jsonPayload)
+      ROOM_JOINED::class.simpleName              -> jsonMapper.readValue<ROOM_JOINED>(jsonPayload)
+      ROOM_FULL::class.simpleName                -> jsonMapper.readValue<ROOM_FULL>(jsonPayload)
+      ROOM_LEFT::class.simpleName                -> jsonMapper.readValue<ROOM_LEFT>(jsonPayload)
+      PLAYER_JOINED::class.simpleName            -> jsonMapper.readValue<PLAYER_JOINED>(jsonPayload)
+      GAME_STARTED::class.simpleName             -> jsonMapper.readValue<GAME_STARTED>(jsonPayload)
+      GAME_ENDED::class.simpleName               -> jsonMapper.readValue<GAME_ENDED>(jsonPayload)
+      PLAYER_REPLACED_WITH_BOT::class.simpleName -> jsonMapper.readValue<PLAYER_REPLACED_WITH_BOT>(jsonPayload)
+      NEXT_ROUND::class.simpleName               -> jsonMapper.readValue<NEXT_ROUND>(jsonPayload)
+      NEXT_TURN::class.simpleName                -> jsonMapper.readValue<NEXT_TURN>(jsonPayload)
+      DEAL_DECK::class.simpleName                -> jsonMapper.readValue<DEAL_DECK>(jsonPayload)
+      DO_PASS_HANDOVER::class.simpleName         -> jsonMapper.readValue<DO_PASS_HANDOVER>(jsonPayload)
+      HAND_OVER_PLAYED::class.simpleName         -> jsonMapper.readValue<HAND_OVER_PLAYED>(jsonPayload)
+      HAND_OVER_TAKEN::class.simpleName          -> jsonMapper.readValue<HAND_OVER_TAKEN>(jsonPayload)
+      TRICK_COLLECTED::class.simpleName          -> jsonMapper.readValue<TRICK_COLLECTED>(jsonPayload)
+      TRICK_FINISHED::class.simpleName           -> jsonMapper.readValue<TRICK_FINISHED>(jsonPayload)
+      DO_TAKE_HANDOVER::class.simpleName         -> jsonMapper.readValue<DO_TAKE_HANDOVER>(jsonPayload)
+      GRILL_PLAYED::class.simpleName             -> jsonMapper.readValue<GRILL_PLAYED>(jsonPayload)
+      CARD_PLAYED::class.simpleName              -> jsonMapper.readValue<CARD_PLAYED>(jsonPayload)
+      AVAILABLE_ROOMS::class.simpleName          -> jsonMapper.readValue<AVAILABLE_ROOMS>(jsonPayload)
+      UPDATED_SCORES::class.simpleName           -> jsonMapper.readValue<UPDATED_SCORES>(jsonPayload)
+      CHAT_MESSAGE::class.simpleName             -> jsonMapper.readValue<CHAT_MESSAGE>(jsonPayload)
+      else                                       -> null
+    }
 
-      val deserializedMessage: Any? = when (messageType) {
-        CONNECTION_CONFIRMATION::class.simpleName  -> jsonMapper.readValue<CONNECTION_CONFIRMATION>(jsonPayload)
-        REGISTERED::class.simpleName               -> jsonMapper.readValue<REGISTERED>(jsonPayload)
-        NAME_TAKEN::class.simpleName               -> jsonMapper.readValue<NAME_TAKEN>(jsonPayload)
-        ROOM_CREATED::class.simpleName             -> jsonMapper.readValue<ROOM_CREATED>(jsonPayload)
-        ROOM_NAME_TAKEN::class.simpleName          -> jsonMapper.readValue<ROOM_NAME_TAKEN>(jsonPayload)
-        ROOM_JOINED::class.simpleName              -> jsonMapper.readValue<ROOM_JOINED>(jsonPayload)
-        ROOM_FULL::class.simpleName                -> jsonMapper.readValue<ROOM_FULL>(jsonPayload)
-        ROOM_LEFT::class.simpleName                -> jsonMapper.readValue<ROOM_LEFT>(jsonPayload)
-        PLAYER_JOINED::class.simpleName            -> jsonMapper.readValue<PLAYER_JOINED>(jsonPayload)
-        GAME_STARTED::class.simpleName             -> jsonMapper.readValue<GAME_STARTED>(jsonPayload)
-        GAME_ENDED::class.simpleName               -> jsonMapper.readValue<GAME_ENDED>(jsonPayload)
-        PLAYER_REPLACED_WITH_BOT::class.simpleName -> jsonMapper.readValue<PLAYER_REPLACED_WITH_BOT>(jsonPayload)
-        NEXT_ROUND::class.simpleName               -> jsonMapper.readValue<NEXT_ROUND>(jsonPayload)
-        NEXT_TURN::class.simpleName                -> jsonMapper.readValue<NEXT_TURN>(jsonPayload)
-        DEAL_DECK::class.simpleName                -> jsonMapper.readValue<DEAL_DECK>(jsonPayload)
-        DO_PASS_HANDOVER::class.simpleName         -> jsonMapper.readValue<DO_PASS_HANDOVER>(jsonPayload)
-        HAND_OVER_PLAYED::class.simpleName         -> jsonMapper.readValue<HAND_OVER_PLAYED>(jsonPayload)
-        HAND_OVER_TAKEN::class.simpleName          -> jsonMapper.readValue<HAND_OVER_TAKEN>(jsonPayload)
-        TRICK_COLLECTED::class.simpleName          -> jsonMapper.readValue<TRICK_COLLECTED>(jsonPayload)
-        TRICK_FINISHED::class.simpleName           -> jsonMapper.readValue<TRICK_FINISHED>(jsonPayload)
-        DO_TAKE_HANDOVER::class.simpleName         -> jsonMapper.readValue<DO_TAKE_HANDOVER>(jsonPayload)
-        GRILL_PLAYED::class.simpleName             -> jsonMapper.readValue<GRILL_PLAYED>(jsonPayload)
-        CARD_PLAYED::class.simpleName              -> jsonMapper.readValue<CARD_PLAYED>(jsonPayload)
-        AVAILABLE_ROOMS::class.simpleName          -> jsonMapper.readValue<AVAILABLE_ROOMS>(jsonPayload)
-        UPDATED_SCORES::class.simpleName           -> jsonMapper.readValue<UPDATED_SCORES>(jsonPayload)
-        CHAT_MESSAGE::class.simpleName             -> jsonMapper.readValue<CHAT_MESSAGE>(jsonPayload)
-        else                                       -> null
+    if (deserializedMessage != null) {
+      Gdx.app.postRunnable {
+        messages.onNext(deserializedMessage)
       }
-
-      if (deserializedMessage != null) {
-        Gdx.app.postRunnable {
-          messages.onNext(deserializedMessage)
-        }
-      } else {
-        log("🕊️🕊️🕊️ SOCKET MESSAGE TYPE NOT RECOGNIZED 🕊️🕊️🕊️", messageType)
-      }
+    } else {
+      log("🕊️🕊️🕊️ SOCKET MESSAGE TYPE NOT RECOGNIZED 🕊️🕊️🕊️", messageType)
     }
   }
 
